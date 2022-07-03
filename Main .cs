418ba8c496766aace528e43bc13161ac934f4942
MohammedReshid1 <93836271+MohammedReshid1@users.noreply.github.com>
2022-07-03 17:59:37 +0300

    Add files via upload

---
+++ Main .cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice_shape
+{
+    
+     class major
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("LET US CALCULATE THE AREA AND PERIMETERS OF SHAPES");
+            Circle circle = new Circle();
+            Console.WriteLine("AREA AND PERIMETER OF A CIRCLE");
+            Console.WriteLine("Enter your radus");
+            circle.radius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The area of the circle is: ");
+            Console.WriteLine(circle.getArea());
+            Console.WriteLine("The Perimeter of the circle is: ");
+            Console.WriteLine(circle.getPerimeter());
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("AREA AND PERIMETER OF A SQUARE");
+            Square square = new Square();
+            Console.WriteLine("Enter your length");
+            square.length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your width");
+            square.width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The area of the square is: ");
+            Console.WriteLine(square.getArea());
+            Console.WriteLine("The Perimeter of the square is: ");
+            Console.WriteLine(square.getPerimeter());
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("AREA AND PERIMETER OF A TRIANGLE");
+            Triangle triangle = new Triangle();
+            Console.WriteLine("Enter your base");
+            triangle.base1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your height");
+            triangle.height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your length 1");
+            triangle.length1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your length 2");
+            triangle.lenght2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The area of the triangle is: ");
+            Console.WriteLine(triangle.getArea());
+            Console.WriteLine("The Perimeter of the triangle is: ");
+            Console.WriteLine(triangle.getPerimeter());
+
+
+
+        }
+    }
+}
---
+++ square .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice_shape
+{
+    class Square : Shape
+    {
+        int _length;
+        int _width;
+        public int length
+
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        public int width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public double getArea()
+        {
+            return width * length;
+
+        }
+        public double getPerimeter()
+        {
+            return 2 * (width + length);
+        }
+    }
+}
